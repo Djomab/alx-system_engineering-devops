1fab34d451c12752dfe6679dbd6f870f0ad76b31
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-21 08:16:19 +0100

    ending...

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -43,7 +43,7 @@ file { 'Nginx server config file':
 		try_files \$uri \$uri/ =404;
 	}
 	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+		rewrite ^ https://github.com/Djomab/ permanent;
 	}
 	error_page 404 /404.html;
 	location = /404.html {
