20083bafabe62284aab332cb7756e1c3e5e94e9d
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-11-02 13:43:22 +0100

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
-exec { 'fix':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+# Corrects phpp extension for php
+exec {'phppFix':
+    path    => '/bin',
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
